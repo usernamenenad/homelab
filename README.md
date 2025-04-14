c138ba3d1583f81ba9400b306f639ebea59853ad
usernamenenad <radovic.nenad158@gmail.com>
2025-04-14 19:59:42 +0200

    Kubernetes & Terraform addition

---
+++ README.md
@@ -0,0 +1,1 @@
+Homelab GitOps files
---
+++ terraform/main.tf
@@ -0,0 +1,31 @@
+terraform {
+    required_providers {
+        hcloud = {
+            source = "hetznercloud/hcloud"
+            version = "~> 1.50.0"
+        }     
+    }
+}
+
+variable "hcloud_token" {
+    type = string
+    sensitive = true
+}
+
+resource "hcloud_ssh_key" "main" {
+  name       = "ssh-key"
+  public_key = file("~/.ssh/id_ed25519.pub")
+}  
+
+provider "hcloud" {
+    token = var.hcloud_token
+}
+
+resource "hcloud_server" "server" {
+    count = 2
+    name = "server-${count.index + 1}"
+    server_type = "cx22"
+    image = "ubuntu-22.04"
+    location = "nbg1"
+    ssh_keys = [hcloud_ssh_key.main.name]
+}
