728db5c6a69cb9ed9bf3e9609af4bf41d8940f93
usernamenenad <radovic.nenad158@gmail.com>
2026-03-01 20:19:00 +0100

    feat: configure istio

--- infra/hetzner/modules/load_balancer/main.tf
+++ infra/hetzner/modules/load_balancer/main.tf
@@ -16,3 +16,10 @@ resource "hcloud_load_balancer_network" "this" {
   load_balancer_id = hcloud_load_balancer.this.id
   network_id       = var.load_balancer_network_id
 }
+
+resource "hcloud_load_balancer_target" "this" {
+  type             = "server"
+  load_balancer_id = hcloud_load_balancer.this.id
+  server_id        = "122442915" # TODO - replace this
+  use_private_ip   = true
+}
